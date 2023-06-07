40e6d38937f859e62327cb32b60ef5b1f0efb85d
ronnygomezp21 <ronny.gomezpe@ug.edu.ec>
2023-06-07 11:07:18 -0500

    cambiso

--- lib/pages/pdf_view_page.dart
+++ lib/pages/pdf_view_page.dart
@@ -6,6 +6,7 @@ import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:intl/intl.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PdfViewPage extends StatefulWidget {
   const PdfViewPage({super.key, required this.url});
@@ -39,23 +40,135 @@ class _PdfViewPageState extends State<PdfViewPage> {
     return file.path;
   }
 
+  Future<void> launchInWebViewOrVC(Uri url) async {
+    if (!await launchUrl(
+      url,
+      mode: LaunchMode.externalApplication,
+      //mode: LaunchMode.externalApplication,
+      //mode: LaunchMode.externalNonBrowserApplication,
+      //mode: LaunchMode.platformDefault,
+      webViewConfiguration: const WebViewConfiguration(
+          headers: <String, String>{'my_header_key': 'my_header_value'}),
+    )) {
+      debugPrint('opening Google docs with PDF url = $url');
+      throw Exception('Could not launch $url');
+    }
+  }
+
   Future<void> savePDF() async {
     var response = await http.get(Uri.parse(widget.url));
-    var status = await Permission.storage.status;
+    var status = await Permission.manageExternalStorage.status;
     if (!status.isGranted) {
-      await Permission.storage.request();
+      await Permission.manageExternalStorage.request();
+      status = await Permission.manageExternalStorage.status;
+      //return;
     }
     if (status.isGranted) {
       var dir = await getExternalStorageDirectory();
       if (dir != null) {
-        File file = File("${dir.path}/data.pdf");
-        await file.writeAsBytes(response.bodyBytes, flush: true);
+        String timestamp = DateFormat('yyyyMMdd_HHmmss').format(DateTime.now());
+        String fileName = 'data_$timestamp.pdf';
+
+        File file = File("${dir.path}/$fileName");
+        file.writeAsBytesSync(response.bodyBytes, flush: true);
+        // File file = File("${dir.path}/data.pdf");
+        // await file.writeAsBytes(response.bodyBytes, flush: true);
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Archivo PDF guardado exitosamente')),
         );
       }
+      print('permiso concedido');
+      print(dir);
+      if (status.isDenied) {
+        print('permiso denegado');
+      }
     }
+    // var response = await http.get(Uri.parse(widget.url));
+    // var status = await Permission.storage.status;
+    // if (!status.isGranted) {
+    //   await Permission.storage.request();
+    //   status = await Permission
+    //       .storage.status; // Verifica nuevamente el estado del permiso
+    // }
+    // if (status.isDenied) {
+    //   await Permission.storage.request();
+    //   status = await Permission.storage.status;
+    // }
+    // if (status.isGranted) {
+    //   Directory? dir = await getDownloadsDirectory();
+    //   if (dir != null) {
+    //     File file = File("${dir.path}/data.pdf");
+    //     await file.writeAsBytes(response.bodyBytes, flush: true);
+    //     ScaffoldMessenger.of(context).showSnackBar(
+    //       SnackBar(content: Text('Archivo PDF guardado exitosamente')),
+    //     );
+    //   } else {
+    //     ScaffoldMessenger.of(context).showSnackBar(
+    //       SnackBar(
+    //           content: Text('No se pudo obtener la ubicación de descarga')),
+    //     );
+    //   }
+    // } else {
+    //   ScaffoldMessenger.of(context).showSnackBar(
+    //     SnackBar(content: Text('No se otorgó el permiso de almacenamiento')),
+    //   );
+    // }
   }
+
+  // Future<void> savePDF() async {
+  //   //var status = await Permission.storage.status;
+  //   PermissionStatus status = await Permission.storage.status;
+  //   if (status.isDenied) {
+  //     // El usuario negó el acceso al almacenamiento
+  //     // Puedes mostrar un mensaje o realizar alguna acción adicional
+  //     debugPrint('Permiso denegado');
+  //     // Solicitar permiso de almacenamiento nuevamente
+  //     status = await Permission.storage.request();
+
+  //     if (status.isDenied) {
+  //       debugPrint('Permiso denegado el acceso de nuevo');
+  //       status = await Permission.storage.request();
+  //       // El usuario volvió a negar el acceso al almacenamiento
+  //       // Puedes mostrar un mensaje o realizar alguna acción adicional
+  //     } else if (status.isPermanentlyDenied) {
+  //       // El usuario negó permanentemente el acceso al almacenamiento
+  //       // Puedes mostrar un mensaje y redirigirlo a la configuración de la aplicación
+  //       await openAppSettings();
+  //     }
+  //   } else if (status.isPermanentlyDenied) {
+  //     // El usuario negó permanentemente el acceso al almacenamiento
+  //     // Puedes mostrar un mensaje y redirigirlo a la configuración de la aplicación
+  //     await openAppSettings();
+  //   } else if (status.isGranted) {
+  //     var response = await http.get(Uri.parse(widget.url));
+  //     var dir = await getDownloadsDirectory();
+  //     if (dir != null) {
+  //       File file = File("${dir.path}/data.pdf");
+  //       await file.writeAsBytes(response.bodyBytes, flush: true);
+  //       ScaffoldMessenger.of(context).showSnackBar(
+  //         SnackBar(content: Text('Archivo PDF guardado exitosamente')),
+  //       );
+  //     }
+  //     // El usuario concedió el permiso de almacenamiento
+  //     // Puedes realizar las acciones necesarias para acceder al almacenamiento
+  //   }
+  //   // if (!status.isGranted) {
+  //   //   await Permission.storage.request();
+  //   // }
+  //   // if (status.isDenied) {
+  //   //   await Permission.storage.request();
+  //   // }
+  //   // if (status.isGranted) {
+  //   //   var dir = await getDownloadsDirectory();
+  //   //   if (dir != null) {
+  //   //     File file = File("${dir.path}/data.pdf");
+  //   //     await file.writeAsBytes(response.bodyBytes, flush: true);
+  //   //     ScaffoldMessenger.of(context).showSnackBar(
+  //   //       SnackBar(content: Text('Archivo PDF guardado exitosamente')),
+  //   //     );
+  //   //   }
+  //   // }
+  // }
 
   @override
   void initState() {
@@ -157,9 +270,12 @@ class _PdfViewPageState extends State<PdfViewPage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          savePDF();
-        },
+        onPressed: savePDF,
+        //   savePDF();
+        //   // launchInWebViewOrVC(
+        //   //   Uri.parse(widget.url),
+        //   // );
+        // },
         child: const Icon(Icons.save),
       ),
     );
