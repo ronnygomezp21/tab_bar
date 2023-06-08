670df4871dc6fb3a58249204727ee57c7fe163e9
ronnygomezp21 <ronny.gomezpe@ug.edu.ec>
2023-06-08 15:01:13 -0500

    cambios

--- lib/modules/home/pages/home_page.dart
+++ lib/modules/home/pages/home_page.dart
@@ -37,7 +37,7 @@ class HomePage extends StatelessWidget {
         ),
         body: const TabBarView(
           children: [
-            DetailPage(),
+            //DetailPage(),
             ArchivePage(),
             ActionPage(),
             TraceabilityPage(),
